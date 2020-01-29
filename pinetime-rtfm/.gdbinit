57ba1aabf6f385e7d267ca32caa07c0ab646f849
Danilo Bargen <mail@dbrgn.ch>
2020-01-29 23:21:40 +0100

    Use github version of HAL

--- pinetime-rtfm/.gdbinit
+++ pinetime-rtfm/.gdbinit
@@ -1,10 +1,17 @@
+# OpenOCD
 target remote :3333
+# JLinkGDBServerExe
+#target remote :2331
 
+# General config
 set backtrace limit 32
 
+# Target config
 #set arm force-mode thumb
 #monitor arm semihosting enable
 
+# Load binary
 load
 break main
 continue
+step
--- pinetime-rtfm/src/main.rs
+++ pinetime-rtfm/src/main.rs
@@ -6,26 +6,23 @@ use panic_semihosting;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-use nrf52832_hal as hal;
+use nrf52832_hal::{self as hal, pac};
 use nrf52832_hal::gpio::Level;
 use nrf52832_hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    //hprintln!("Hello PineTime from Rust!").unwrap();
+    let _p = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
-    let p = cortex_m::Peripherals::take().unwrap();
-    let dp = hal::nrf52832_pac::Peripherals::take().unwrap();
-
-    let gpio = dp.P0.split();
+    let gpio = hal::gpio::p0::Parts::new(dp.P0);
 
     let mut backlight_low = gpio.p0_14.into_push_pull_output(Level::High);
     let mut backlight_mid = gpio.p0_22.into_push_pull_output(Level::High);
     let mut backlight_high = gpio.p0_23.into_push_pull_output(Level::High);
-    backlight_low.set_low();
-    backlight_mid.set_low();
-    backlight_high.set_low();
+    backlight_low.set_low().unwrap();
+    backlight_mid.set_low().unwrap();
+    backlight_high.set_low().unwrap();
 
     loop {
         asm::nop();
